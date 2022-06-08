669119bbc3c03ba5ef4791e677b6a55236c77095
aquaHsu <bellems811@gmail.com>
2022-06-08 12:08:15 +0800

    first commit

--- CMakeFiles/myTarget.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/myTarget"
-  "msg_pb2.py"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/myTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
